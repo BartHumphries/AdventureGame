32462b1f64f498bef03346c9ad5b9f44b4c6dce5
Jack <Jack@192.168.2.3>
2016-10-06 21:09:51 -0400

    Merge origin/master
    Conflicts:
    	build.xml
    	manifest.mf
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	src/adventuregame/AdventureGame.java

--- src/byui/cit260/adventureGame/model/Character.java
+++ src/byui/cit260/adventureGame/model/Character.java
@@ -16,17 +16,16 @@ public class Character implements Serializable{
     
     // class instance variables
     private String name;
-    private double bestTime;
+    private double level;
+    private boolean health;
+    private String faction;
+    private int attackPhysical;
+    private int defensePhysical;
+    private int attackMagical;
+    private int defenseMagical;
 
     public Character() {
     }
-
-    public Character(String name, double bestTime) {
-        this.name = name;
-        this.bestTime = bestTime;
-    }
- 
-    
     
     public String getName() {
         return name;
@@ -36,25 +35,74 @@ public class Character implements Serializable{
         this.name = name;
     }
 
-    public double getBestTime() {
-        return bestTime;
+    public double getLevel() {
+        return level;
     }
 
-    public void setBestTime(double bestTime) {
-        this.bestTime = bestTime;
+    public void setLevel(double level) {
+        this.level = level;
+    }
+
+    public boolean isHealth() {
+        return health;
+    }
+
+    public void setHealth(boolean health) {
+        this.health = health;
+    }
+
+    public String getFaction() {
+        return faction;
+    }
+
+    public void setFaction(String faction) {
+        this.faction = faction;
+    }
+
+    public int getAttackPhysical() {
+        return attackPhysical;
+    }
+
+    public void setAttackPhysical(int attackPhysical) {
+        this.attackPhysical = attackPhysical;
+    }
+
+    public int getDefensePhysical() {
+        return defensePhysical;
+    }
+
+    public void setDefensePhysical(int defensePhysical) {
+        this.defensePhysical = defensePhysical;
+    }
+
+    public int getAttackMagical() {
+        return attackMagical;
+    }
+
+    public void setAttackMagical(int attackMagical) {
+        this.attackMagical = attackMagical;
+    }
+
+    public int getDefenseMagical() {
+        return defenseMagical;
+    }
+
+    public void setDefenseMagical(int defenseMagical) {
+        this.defenseMagical = defenseMagical;
     }
 
     @Override
     public int hashCode() {
         int hash = 5;
-        hash = 89 * hash + Objects.hashCode(this.name);
-        hash = 89 * hash + (int) (Double.doubleToLongBits(this.bestTime) ^ (Double.doubleToLongBits(this.bestTime) >>> 32));
+        hash = 71 * hash + Objects.hashCode(this.name);
+        hash = 71 * hash + (int) (Double.doubleToLongBits(this.level) ^ (Double.doubleToLongBits(this.level) >>> 32));
+        hash = 71 * hash + (this.health ? 1 : 0);
+        hash = 71 * hash + Objects.hashCode(this.faction);
+        hash = 71 * hash + this.attackPhysical;
+        hash = 71 * hash + this.defensePhysical;
+        hash = 71 * hash + this.attackMagical;
+        hash = 71 * hash + this.defenseMagical;
         return hash;
-    }
-
-    @Override
-    public String toString() {
-        return "Character{" + "name=" + name + ", bestTime=" + bestTime + '}';
     }
 
     @Override
@@ -69,14 +117,32 @@ public class Character implements Serializable{
             return false;
         }
         final Character other = (Character) obj;
-        if (Double.doubleToLongBits(this.bestTime) != Double.doubleToLongBits(other.bestTime)) {
+        if (Double.doubleToLongBits(this.level) != Double.doubleToLongBits(other.level)) {
+            return false;
+        }
+        if (this.health != other.health) {
+            return false;
+        }
+        if (this.attackPhysical != other.attackPhysical) {
+            return false;
+        }
+        if (this.defensePhysical != other.defensePhysical) {
+            return false;
+        }
+        if (this.attackMagical != other.attackMagical) {
+            return false;
+        }
+        if (this.defenseMagical != other.defenseMagical) {
             return false;
         }
         if (!Objects.equals(this.name, other.name)) {
             return false;
         }
-        return true;
+        return Objects.equals(this.faction, other.faction);
     }
-    
-    
+
+    @Override
+    public String toString() {
+        return "Character{" + "name=" + name + ", level=" + level + ", health=" + health + ", faction=" + faction + ", attackPhysical=" + attackPhysical + ", defensePhysical=" + defensePhysical + ", attackMagical=" + attackMagical + ", defenseMagical=" + defenseMagical + '}';
+    }
 }
