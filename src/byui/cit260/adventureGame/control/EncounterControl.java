48edafd007ddf733b5efe33b737b5ed2086112b4
vladbalashov <vladbalashov@192.168.1.5>
2016-10-22 22:59:47 +0300

    i added code to calcAbilityToWinFunction - 10/22/2016

--- src/byui/cit260/adventureGame/control/EncounterControl.java
+++ src/byui/cit260/adventureGame/control/EncounterControl.java
@@ -22,6 +22,54 @@ public String getReward(String weapon, String potion, String ingredients) {
    return "This is the return statement from getReward()\n";      
 }
 public String addToCharacter(String item, int amount) {
-   return "This is the return statement from addToCharacter()\n";      
+   return "This is the return statement from addToCharacter()\n"; 
+}
+public double calcAbilityToWin(double attackPhysical1, double attackMagical1,
+        double defensePhysical1, double defenseMagical1, double attackPhysical2, double attackMagical2,
+        double defensePhysical2, double defenseMagical2, double health1, double health2 ) {
+    
+    if (attackPhysical1<0 || attackMagical1<0 || defensePhysical1<0 || defenseMagical1<0
+    || attackPhysical2<0 || attackMagical2<0 || defensePhysical2<0 || defenseMagical2<0) {
+        return -1000000;
+    }
+    if (health1<=0 || health1>10000 || health2<=0 || health2>10000) {
+        return -1000000;
+    }
+    
+    double attack1 = attackPhysical1 + attackMagical1;
+    double defense1 = defensePhysical1 + defenseMagical1;
+    double attack2 = attackPhysical2 + attackMagical2;
+    double defense2 = defensePhysical2 + defenseMagical2;
+    double check1 = attack1 - defense2;
+    double check2 = attack2 - defense1;
+    
+    if (check1 == 0 && check2 == 0) {
+        double level = 0;
+        return level;
+    }
+
+    if (check1==0 && check2>0){
+        double level = 0 - health1/check2;
+        return level;
+    }
+
+    if (check1==0 && check2<0){
+        double level = 0;
+        return level;
+    }
+
+    if (check2==0 && check1>0) {
+        double level = health2/check1;
+        return level;
+    }
+
+    if (check2==0 && check1<0) {
+        double level = 0;
+        return level;
+    }
+
+    double level = health2/check1 - health1/check2;
+    return level;
+   
 }
 }
