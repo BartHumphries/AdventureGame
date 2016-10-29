2d0577a488e24987e2a8f63368d5fc86c7c9815b
vladbalashov <vladbalashov@mbpvladbalashov>
2016-10-29 23:38:58 +0300

    Main class changes - 10/29

--- src/adventuregame/AdventureGame.java
+++ src/adventuregame/AdventureGame.java
@@ -8,47 +8,39 @@ import byui.cit260.adventureGame.model.Character;
 import byui.cit260.adventureGame.model.Game;
 import byui.cit260.adventureGame.model.Map;
 import byui.cit260.adventureGame.model.PartyComposition;
+import byui.cit260.adventureGame.model.Player;
+import byui.cit260.adventureGame.view.StartGameView;
 
 /**
  *
  * @author Jack
  */
 public class AdventureGame {
+    
+    private static Game currentGame = null;
+    private static Player player = null;
 
-    /**
-     * @param args the command line arguments
-     */
+    public static Game getCurrentGame() {
+        return currentGame;
+    }
+
+    public static void setCurrentGame(Game currentGame) {
+        AdventureGame.currentGame = currentGame;
+    }
+
+    public static Player getPlayer() {
+        return player;
+    }
+
+    public static void setPlayer(Player player) {
+        AdventureGame.player = player;
+    }
+
+    
     public static void main(String[] args) {
-        Character playerOne = new Character();
         
-        playerOne.setName("Bart");
-        playerOne.setLevel(7.00);
-        
-        String playerInfo = playerOne.toString();
-        System.out.println(playerInfo);
-        
-        Character Dude = new Character();
-        Character Dude2 = new Character();
-        
-        PartyComposition partyOne = new PartyComposition();
-        Character[] tempCharList ={Dude, Dude2};
-        partyOne.setCharacterList(tempCharList);
-        
-        Game gameOne = new Game();
-        
-        gameOne.setCurrentLocation("Fight Land");
-        gameOne.setSaveSlot(1);
-        
-        String gameInfo = gameOne.toString();
-        System.out.println(gameInfo);
-        
-        Map locationOne = new Map();
-        
-        locationOne.setLocationList("Miracle Land");
-        
-        String locationInfo = locationOne.toString();
-        System.out.println(locationInfo);
-        
+        StartGameView startGameView = new StartGameView();
+        startGameView.displayStartGameView();
     }
     
 }
